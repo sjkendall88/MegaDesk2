b6f907043fec43eb136c2417503bc3a69e1dcd77
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-10-19 14:16:27 -0600

    added keyboard ability for search page

--- MegaDesk-SterlingKendall/MegaDesk-SterlingKendall/SearchQuotes.Designer.cs
+++ MegaDesk-SterlingKendall/MegaDesk-SterlingKendall/SearchQuotes.Designer.cs
@@ -32,7 +32,6 @@
             this.BackButton = new System.Windows.Forms.Button();
             this.subButton = new System.Windows.Forms.Button();
             this.searchComboBox = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.searchGridView = new System.Windows.Forms.DataGridView();
             this.fName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.lName = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -57,39 +56,28 @@
             this.BackButton.Name = "BackButton";
             this.BackButton.Size = new System.Drawing.Size(155, 56);
             this.BackButton.TabIndex = 3;
-            this.BackButton.Text = "Main Menu";
+            this.BackButton.Text = "&Main Menu";
             this.BackButton.UseVisualStyleBackColor = true;
             this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
             // 
             // subButton
             // 
             this.subButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.subButton.Location = new System.Drawing.Point(327, 76);
+            this.subButton.Location = new System.Drawing.Point(149, 75);
             this.subButton.Name = "subButton";
             this.subButton.Size = new System.Drawing.Size(88, 31);
             this.subButton.TabIndex = 6;
-            this.subButton.Text = "Search";
+            this.subButton.Text = "&Search";
             this.subButton.UseVisualStyleBackColor = true;
             this.subButton.Click += new System.EventHandler(this.SubButton_Click);
             // 
             // searchComboBox
             // 
             this.searchComboBox.FormattingEnabled = true;
-            this.searchComboBox.Location = new System.Drawing.Point(200, 85);
+            this.searchComboBox.Location = new System.Drawing.Point(19, 85);
             this.searchComboBox.Name = "searchComboBox";
             this.searchComboBox.Size = new System.Drawing.Size(121, 21);
             this.searchComboBox.TabIndex = 7;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(52, 86);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 20);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Search By Material";
             // 
             // searchGridView
             // 
@@ -186,7 +174,6 @@
             this.Controls.Add(this.NewQuoteLabel);
             this.Controls.Add(this.searchResult);
             this.Controls.Add(this.searchGridView);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.searchComboBox);
             this.Controls.Add(this.subButton);
             this.Controls.Add(this.BackButton);
@@ -206,7 +193,6 @@
         private System.Windows.Forms.BindingSource deskBindingSource;
         private System.Windows.Forms.Button subButton;
         private System.Windows.Forms.ComboBox searchComboBox;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView searchGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn fName;
         private System.Windows.Forms.DataGridViewTextBoxColumn lName;
